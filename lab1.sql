3d6d50cd3563d20d3d31934db8d7c23795deb45c
Ines Rondeau <inesrondeau99@gmail.com>
2021-08-17 17:17:54 +0200

    lab1

---
+++ lab1.sql
@@ -0,0 +1,12 @@
+select *
+FROM sakila.actor, sakila.address, sakila.category, sakila.city, sakila.country, sakila.customer, sakila.film, sakila.film_actor, sakila.film_category, sakila.film_text, sakila.inventory, sakila.language, sakila.payment, sakila.rental, sakila.staff, sakila.store;
+select title 
+FROM sakila.film;
+select distinct lenguage_id as L
+FROM lenguage;
+
+
+
+
+
+
